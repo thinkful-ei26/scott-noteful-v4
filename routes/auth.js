ee2984e652d748256e8f4d9868a9a12cf03ade61
Scott Ian <sim2487@gmail.com>
2018-12-04 13:04:14 -0800

    Add JWT to auth route, fix config require;

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,25 @@
 "use strict";
 const express = require("express");
 const passport = require("passport");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
+
+const { JWT_SECRET, JWT_EXPIRY } = require("../config");
 
 const options = {session: false, failWithError: true};
 
 const localAuth = passport.authenticate('local', options);
 
+function createAuthToken (user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
+
 router.post('/login', localAuth, function (req, res) {
-  return res.json(req.user);
+  const authToken = createAuthToken(req.user);
+  res.json({ authToken });
 });
 
 module.exports = router;
