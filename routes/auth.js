0ab74b065f4350e7d3cf1a4b6e8b975f147acfcc
Scott Ian <sim2487@gmail.com>
2018-12-04 14:13:01 -0800

    add refresh endpoint and update client

--- routes/auth.js
+++ routes/auth.js
@@ -22,4 +22,11 @@ router.post('/login', localAuth, function (req, res) {
   res.json({ authToken });
 });
 
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
+
+router.post('/refresh', jwtAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  res.json({ authToken });
+});
+
 module.exports = router;
